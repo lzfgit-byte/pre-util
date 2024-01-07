07e6ced8ceaa6b1bd6860a56bf2d6b3b4bf67fb7
lzfgit-byte <1807409742@qq.com>
2024-01-07 17:32:09 +0800

    feat:init

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+      copyDtsFiles: false,
+    }),
+  ],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'lib',
+      formats: ['es', 'umd'],
+      // the proper extensions will be added
+      fileName: 'lib',
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'), // 路径别名
+    },
+    extensions: ['.js', '.vue', '.json', '.ts'], // 使用路径别名时想要省略的后缀名，可以自己 增减
+  },
+});
