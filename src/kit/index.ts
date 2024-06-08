d9ebcb174e835112e37d7223d2f99d90877060fe
lzfgit-byte <1807409742@qq.com>
2024-06-08 16:09:51 +0800

    feat:init

--- src/kit/index.ts
+++ src/kit/index.ts
@@ -1,9 +1,19 @@
+import { Md5 } from 'ts-md5';
+/**
+ *执行一个方法
+ * @param func
+ * @param args
+ */
 export const executeFunc = (func: Function, ...args: any[]) => {
   try {
     func && func(...args);
   } catch (e) {}
 };
 
+/**
+ *等待时间，默认是200毫秒
+ * @param delay
+ */
 export const waitTime = (delay = 200) => {
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -11,5 +21,16 @@ export const waitTime = (delay = 200) => {
     }, delay);
   });
 };
-
+/**
+ * 获取一个随机key
+ * @param code
+ */
 export const generateKey = (code?: string) => new Date().getTime() + code;
+
+/**
+ *求字符串的hash值
+ * @param str
+ */
+export const hashString = (str: string) => {
+  return Md5.hashStr(str);
+};
